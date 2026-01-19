f8c9554910d95749809fc7ba108f6012914184bf
Raiuri <raiuresantos@ymail.com>
2026-01-19 16:05:33 -0300

    refact: remove localhost from BASE_URL variable

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { PredictionRequest, PredictionResponse } from '../types/dashboard';
 
 // Pega URL do ambiente ou usa localhost como padrão
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
     baseURL: BASE_URL,
